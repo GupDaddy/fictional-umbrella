7cd2de8b6898fb7de2d92c8ea00db23bf8702b36
Chris Guthrie <guthricb@gmail.com>
2019-06-08 20:03:02 +0000

    resize photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_183142) do
+ActiveRecord::Schema.define(version: 2019_06_08_190506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_06_08_183142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["place_id"], name: "index_photos_on_place_id"
   end
 
