214fc25c9da7c543862692739bb4879d05c0d9d8
Chris Guthrie <guthricb@gmail.com>
2019-06-09 23:41:26 +0000

    Lesson 41 - Previewing the email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
 	default from: 'no-reply@nomsterapp.com'
 
 	def comment_added
-		mail(to: "nashville@amateurgolftour.net",
+		mail(to: "guthricb@gmail.com",
 			 subject: "A comment has been added to your place")
 	end
 end
